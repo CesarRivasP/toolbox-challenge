2b9207dcde78209fef091ddec95e8c891c449e4f
CesarRivasP <cesarjose58@gmail.com>
2022-03-02 11:25:19 -0400

    fix: delete without using code

--- src/config/envVars.js
+++ src/config/envVars.js
@@ -1,8 +1,4 @@
 import Constants from 'expo-constants';
-// import {
-//   NODE_ENV,
-//   API_URL,
-// } from '@env';
 
 let ENVS;
 
@@ -13,12 +9,5 @@ if (Constants?.appOwnership === 'expo') {
     ACCESS_USER_KEY: expoContants?.ACCESS_USER_KEY || 'defaultKey',
   };
 }
-// TODO: Implement in bare workflow
-// else {
-//   ENVS = {
-//     NODE_ENV: NODE_ENV || '',
-//     API_URL: API_URL || '',
-//   };
-// }
 
 export default ENVS;
